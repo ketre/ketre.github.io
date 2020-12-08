10fcb276d86e3c4a16cea0d69ed055a5deafa807
Anna Ketre <anna.ketre@gmail.com>
2020-12-08 16:23:57 +0000

    newDate

--- sketch.js
+++ sketch.js
@@ -7,11 +7,11 @@ jan1st = +new Date('2020-01-01')
 function draw() {
   background('white');
   for(d = 0; d <366; ++d) {
-    myDate = jan1st + d * 1000*3600*24; 
-    y= new Date(myDate).getMonth()
-    x= new Date(myDate).getDate()
-    rect(x *60, 60 * y, 50,  120)
-    z = new Date(myDate).getDay()
+    myDate = new Date(jan1st + d * 1000*3600*24); 
+    y= myDate.getMonth()
+    x= myDate.getDate()
+    rect(x *60, 60 * y, 50,  80)
+    z = myDate.getDay()
     
     switch(z) {
    case 6:
